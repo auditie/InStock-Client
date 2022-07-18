505bb58cc498310c6d85bca95ae1c32fdccb276d
Audun <audunyoung@gmail.com>
2022-07-18 17:20:40 -0600

    Set up warehouse list item component.

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,17 +1,21 @@
-// import './WarehouseList.scss';
-// // import { Link } from 'react-router-dom';
+import './WarehouseList.scss';
+// import { Link } from 'react-router-dom';
+import WarehouseItem from '../WarehouseItem/WarehouseItem';
 
-// const WarehouseList = ( { } ) => {
-//     return (
-//         <div className='warehouse-list'>
-//             <div className='warehouse-list__head'>
-//                 <h1 className='warehouse-list__head-title'>Warehouses</h1>
-//                 <input type='text' placeholder='Search...' className='warehouse-list__head-search'></input>
-//                 <button className='warehouse-list__head-cta'>+ Add New Warehouse</button>
-//             </div>
+const WarehouseList = ( { } ) => {
+    return (
+        <div className='warehouse-list'>
+            <div className='warehouse-list__head'>
+                <h1 className='warehouse-list__head-title'>Warehouses</h1>
+                <input type='text' placeholder='Search...' className='warehouse-list__head-search'></input>
+                <button className='warehouse-list__head-cta'>+ Add New Warehouse</button>
+            </div>
+            <div className='warehouse-list__warehouses'>
+                <WarehouseItem />
+            </div>
 
-//         </div>
-//     )
-// };
+        </div>
+    )
+};
 
-// export default WarehouseList;
+export default WarehouseList;
