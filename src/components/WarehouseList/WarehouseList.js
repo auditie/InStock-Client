88f4c7f5335f22f5f733314ca34f2c5632165344
Audun <audunyoung@gmail.com>
2022-07-18 16:01:21 -0600

    Hide my code.

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,12 +1,17 @@
-import './WarehouseList.scss';
-// import { Link } from 'react-router-dom';
+// import './WarehouseList.scss';
+// // import { Link } from 'react-router-dom';
 
-const WarehouseList = ( { } ) => {
-    return (
-        <div className='warehouse-list'>
+// const WarehouseList = ( { } ) => {
+//     return (
+//         <div className='warehouse-list'>
+//             <div className='warehouse-list__head'>
+//                 <h1 className='warehouse-list__head-title'>Warehouses</h1>
+//                 <input type='text' placeholder='Search...' className='warehouse-list__head-search'></input>
+//                 <button className='warehouse-list__head-cta'>+ Add New Warehouse</button>
+//             </div>
 
-        </div>
-    )
-};
+//         </div>
+//     )
+// };
 
-export default WarehouseList;
+// export default WarehouseList;
