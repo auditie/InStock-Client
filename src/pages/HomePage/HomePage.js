1655b06a60a43c4caad7652ec993bc14aa04257e
Audun <audunyoung@gmail.com>
2022-07-19 16:39:09 -0600

    Set up state and axios request.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,19 +3,36 @@ import './HomePage.scss';
 import { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import WarehouseList from '../../components/WarehouseList/WarehouseList';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
 
 class HomePage extends Component {
     state = {
         warehouses: [],
+        warehouseInventory: [],
         selectedWarehouse: {}
     };
 
     // set up axios
+    componentDidMount() {
+        axios.get(`${API_URL}/warehouses`)
+        .then(response => {
+            // console.log(response.data);
+            this.setState({
+                warehouses: response.data
+            })
+        })
+    }
+
 
     render() {
         return (
             <Switch> 
-                <Route path='/' component={WarehouseList}/>
+                <Route path='/' exact component={WarehouseList}/>
+                {/* <Route path='/:warehouseId' component={} /> */}
+                {/* <Route path='/warehouses/:warehouseId/edit' component={} /> */}
+                {/* <Route path='/warehouses/add' component={HomePage} /> */}
             </Switch>
         )
     }
