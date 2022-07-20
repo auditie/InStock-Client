33d00c032021806d441a7b0a6153012b464a77ec
chelsia tong <chelsiatong@gmail.com>
2022-07-19 23:15:02 -0400

    added front end add warehouse .js and scss styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import AddWarehouse from './components/AddWarehouse/AddWarehouse';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import HomePage from './pages/HomePage/HomePage';
 // import InventoryPage from './pages/InventoryPage/InventoryPage';
@@ -12,6 +13,7 @@ function App() {
             <Header />
             <div className="app__bodywrap">
                 <div>
+                    <AddWarehouse />
                     {/* this is where the white panel is basically that has all the information for warehouse, inventory, etc. */}
                     {/* <Switch>
                         <Route path='/' exact component={HomePage} />
