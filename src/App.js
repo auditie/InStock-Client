a575241aef225cdebe52ea402ff1b3904737e590
stephonduong <stephonduong@gmail.com>
2022-07-20 19:29:26 -0400

    single inventory page, meant to be in feature/M22TOBK-17 branch

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 // import AddWarehouse from './components/AddWarehouse/AddWarehouse';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-// import InventoryPage from './pages/InventoryPage/InventoryPage';
+// import HomePage from './pages/HomePage/HomePage';
+import InventoryPage from './pages/InventoryPage/InventoryPage';
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
             <Header />
             <div className="app__bodywrap">
                 <div className="app__card" >
-                    <HomePage />
+                    <InventoryPage />
                     {/* this is where the white panel is basically that has all the information for warehouse, inventory, etc. */}
                     {/* <Switch>
                         <Route path='/' exact component={HomePage} />
