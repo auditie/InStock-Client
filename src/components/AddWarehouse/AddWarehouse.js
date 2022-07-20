8ab15c6456027afff0817ae15daa78b6a23390cc
chelsia tong <chelsiatong@gmail.com>
2022-07-19 21:10:55 -0400

    created new feature branch for adding warehouse

---
+++ src/components/AddWarehouse/AddWarehouse.js
@@ -0,0 +1,20 @@
+import './AddWarehouse.scss';
+import React from 'react';
+
+class AddWarehouse extends React.Component {
+    state = {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default AddWarehouse;
