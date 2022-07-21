d6117324819f76fe3bf6f26cd0e1b783096ec309
stephonduong <stephonduong@gmail.com>
2022-07-21 11:06:48 -0400

    added warehouse list scss

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,19 +1,24 @@
 import './WarehouseList.scss';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import WarehouseItem from '../WarehouseItem/WarehouseItem';
 
-const WarehouseList = (props) => {
+const WarehouseList = ( { warehousesList } ) => {
+    console.log(warehousesList)
     return (
-        <div className='warehouse-list'>
-            {/*(<div className='warehouse-list__head'>
+        <>
+            <div className='warehouse-list__head'>
                 <h1 className='warehouse-list__head-title'>Warehouses</h1>
                 <input type='text' placeholder='Search...' className='warehouse-list__head-search'></input>
-    <button className='warehouse-list__head-cta'>+ Add New Warehouse</button>
-    </div>*/}
-                <Link to={'/warehouses/' + props.id} onClick={props.handleClick}>
-                hello
-                </Link>
-
-        </div>
+                <button className='warehouse-list__head-cta'>+ Add New Warehouse</button>
+            </div>
+            <div className='warehouse-list__warehouses'>
+            {/* {
+                warehouses.map( warehouse => {
+                    <WarehouseItem key={warehouse.id} warehouse={warehouse} />
+                })
+            } */}
+            </div>
+        </>
     )
 };
 
