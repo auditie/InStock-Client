6392ccb34772a6c79665b59b5f6f0f12bc6ed66b
chelsia tong <chelsiatong@gmail.com>
2022-07-21 18:03:11 -0400

    adding errors for required fields

--- src/components/AddWarehouse/AddWarehouse.js
+++ src/components/AddWarehouse/AddWarehouse.js
@@ -16,6 +16,8 @@ class AddWarehouse extends React.Component {
         contactPhone: "",
         contactEmail: "",
         invalidInput: false,
+        // warning: {},
+        // fields:{},
     };
 
     addWarehouse = (event) => {
@@ -24,8 +26,7 @@ class AddWarehouse extends React.Component {
 
         if (!this.state.name || !this.state.address || !this.state.city || !this.state.country || !this.state.contactName || !this.state.contactPosition || !this.state.contactPhone || !this.state.contactEmail) {
             this.setState({
-                invalidInput: true
-
+                invalidInput: true,
             })
         }
         axios
@@ -66,24 +67,33 @@ class AddWarehouse extends React.Component {
                             <h2 className="add-warehouse__subtitle" >Warehouse Details</h2>
                             <h3 className="add-warehouse__labels" >Warehouse Name</h3>
                             <textarea className={
-                                (this.state.name.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="Warehouse Name" value={this.state.name} onChange={this.handleChangeWarehouse} name="name" ></textarea>
+                                (this.state.name.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="Warehouse Name" value={this.state.name} onChange={this.handleChangeWarehouse} name="name">
+                            </textarea>
+                            <span className="add-warehouse__warning" aria-live="polite">This field is required</span>
                             <h3 className="add-warehouse__labels" >Street Address</h3>
-                            <textarea type="text" placeholder="Street Address" value={this.state.address} onChange={this.handleChangeWarehouse} name="address" ></textarea>
+                            <textarea className={
+                                (this.state.address.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="Street Address" value={this.state.address} onChange={this.handleChangeWarehouse} name="address" ></textarea>
                             <h3 className="add-warehouse__labels" >City</h3>
-                            <textarea type="text" placeholder="City" value={this.state.city} onChange={this.handleChangeWarehouse} name="city" ></textarea>
+                            <textarea className={
+                                (this.state.city.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="City" value={this.state.city} onChange={this.handleChangeWarehouse} name="city" ></textarea>
                             <h3 className="add-warehouse__labels" >Country</h3>
-                            <textarea type="text" placeholder="Country" value={this.state.country} onChange={this.handleChangeWarehouse} name="country" ></textarea>
+                            <textarea className={
+                                (this.state.country.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="Country" value={this.state.country} onChange={this.handleChangeWarehouse} name="country" ></textarea>
                         </div>
                         <div className="add-warehouse__contact--detail" >
                             <h2 className="add-warehouse__subtitle" >Contact Details</h2>
                             <h3 className="add-warehouse__labels" >Contact Name</h3>
-                            <textarea type="text" placeholder="Contact Name" value={this.state.contactName} onChange={this.handleChangeWarehouse} name="contactName" ></textarea>
+                            <textarea className={
+                                (this.state.contactName.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="Contact Name" value={this.state.contactName} onChange={this.handleChangeWarehouse} name="contactName" ></textarea>
                             <h3 className="add-warehouse__labels" >Position</h3>
-                            <textarea type="text" placeholder="Position" value={this.state.contactPosition} onChange={this.handleChangeWarehouse} name="contactPosition" ></textarea>
+                            <textarea className={
+                                (this.state.contactPosition.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="text" placeholder="Position" value={this.state.contactPosition} onChange={this.handleChangeWarehouse} name="contactPosition" ></textarea>
                             <h3 className="add-warehouse__labels" >Phone Number</h3>
-                            <textarea type="tel" placeholder="Phone Number" pattern="[0-9]{3} [0-9]{3} [0-9]{4}" maxLength="12" value={this.state.contactPhone} onChange={this.handleChangeWarehouse} name="contactPhone" ></textarea>
+                            <textarea className={
+                                (this.state.contactPhone.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="tel" placeholder="Phone Number" pattern="[0-9]{3} [0-9]{3} [0-9]{4}" maxLength="12" value={this.state.contactPhone} onChange={this.handleChangeWarehouse} name="contactPhone" ></textarea>
                             <h3 className="add-warehouse__labels" >Email</h3>
-                            <textarea type="email" placeholder="Email" value={this.state.contactEmail} onChange={this.handleChangeWarehouse} name="contactEmail" ></textarea>
+                            <textarea className={
+                                (this.state.contactEmail.length === 0 && this.state.invalidInput) ? "add-warehouse__error" : ""} type="email" placeholder="Email" value={this.state.contactEmail} onChange={this.handleChangeWarehouse} name="contactEmail" ></textarea>
                         </div>
                     </div>
                     <div className="add-warehouse__submit" >
