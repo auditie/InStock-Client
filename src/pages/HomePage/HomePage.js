c45fdf2be2162e64b606ce87b9341a0a6aca8269
stephonduong <stephonduong@gmail.com>
2022-07-21 10:56:17 -0400

    added new HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,100 +1,69 @@
-import './HomePage.scss';
-import axios from 'axios';
-import { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import WarehouseList from '../../components/WarehouseList/WarehouseList';
-import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
+  import './HomePage.scss';
+  import axios from 'axios';
+  import { Component } from 'react';
+  import { Route, Switch } from 'react-router-dom';
+  import WarehouseList from '../../components/WarehouseList/WarehouseList';
+  //import axios from 'axios';
+  
+  const API_URL = 'http://localhost:8080';
+  
+  class HomePage extends Component {
+      state = {
+          warehouses: [],
+          warehouseInventory: [],
+          selectedWarehouse: {}
+      };
 
-class HomePage extends Component {
-  state = {
-    warehouses: [],
-    selectedWarehouse: {
-      id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
-      name: "Manhattan",
-      address: "503 Broadway",
-      city: "New York",
-      country: "USA",
-      contact: {
-        name: "Parmin Aujla",
-        position: "Warehouse Manager",
-        phone: "+1 (646) 123-1234",
-        email: "paujla@instock.com"
-      }
-    },
-    warehouseInventory: [
-      {
-        id: "9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3",
-        warehouseID: "2922c286-16cd-4d43-ab98-c79f698aeab0",
-        warehouseName: "Manhattan",
-        itemName: "Television",
-        description: "This 50\", 4K LED TV provides a crystal-clear picture and vivid colors.",
-        category: "Electronics",
-        status: "In Stock",
-        quantity: 500
-      },
-      {
-        "id": "83433026-ca32-4c6d-bd86-a39ee8b7303e",
-        "warehouseID": "2922c286-16cd-4d43-ab98-c79f698aeab0",
-        "warehouseName": "Manhattan",
-        "itemName": "Gym Bag",
-        "description": "Made out of military-grade synthetic materials, this gym bag is highly durable, water resistant, and easy to clean.",
-        "category": "Gear",
-        "status": "Out of Stock",
-        "quantity": 0
-      }
-    ]
-  };
+        // set up axios
+    getWarehouse = (id) => {
+      axios.get(`http://localhost:8080/warehouses/${id}`)
+        .then((response) => {
+          this.setState({
+            selectedWarehouse: response.data
+          })
+        })
+    }
 
-  // set up axios
-  getWarehouse = (id) => {
-    axios.get(`http://localhost:8080/warehouses/${id}`)
+    handleClick = () => {
+      const warehouseId = this.props.match.params.warehouseId
+      this.getVideo(warehouseId)
+      axios.get(`http://localhost:8080/warehouses/${warehouseId}/inventory`)
       .then((response) => {
         this.setState({
-          selectedWarehouse: response.data
+          warehouseInventory: response.data
         })
       })
+    }
+  
+      // set up axios
+      componentDidMount() {
+          axios.get(`${API_URL}/warehouses`)
+          .then(response => {
+              //console.log(response.data);
+              this.setState({
+                  warehouses: response.data
+              })
+              return response.data;
+          })
+      }
+  
+      render() {
+          if (!this.state.warehouses) {
+              return (
+                  <p>Loading...</p>
+              )
+          }
+          console.log(this.state.warehouses);
+          return (
+              <Switch> 
+                  <Route path='/' exact component={WarehouseList} warehousesList={this.state.warehouses} />
+                  {/* <Route path='/:warehouseId' component={} /> */}
+                  {/* <Route path='/warehouses/:warehouseId/edit' component={} /> */}
+                  {/* <Route path='/warehouses/add' component={HomePage} /> */}
+              </Switch>
+  
+          )
+      }
   }
-
-  handleClick = () => {
-    const warehouseId = this.props.match.params.warehouseId
-    this.getVideo(warehouseId)
-    axios.get(`http://localhost:8080/warehouses/${warehouseId}/inventory`)
-    .then((response) => {
-      this.setState({
-        warehouseInventory: response.data
-      })
-    })
-  }
-
-  componentDidMount() {
-    axios.get(`http://localhost:8080/warehouses`)
-    .then((response => {
-      this.setState({
-        warehouses: response.data
-      })
-    }))
-  }
-
-  render() {
-          {/*<Switch> 
-          <Route 
-              path='/' exact
-              render={(routerProps) => (
-                  <WarehouseList handleClick={this.handleClick} id={this.state.selectedWarehouse.id} {...routerProps}/>
-              )}/>
-          <Route 
-              path='/warehouses/:warehouseId' 
-              render={(routerProps) => (
-                  <WarehouseDetails warehouse={this.state.selectedWarehouse} contact={this.state.selectedWarehouse.contact} inventory={this.state.warehouseInventory} {...routerProps}/>
-              )}/>
-              </Switch>*/}
-    return (
-      <div className='card'>
-      <WarehouseDetails warehouse={this.state.selectedWarehouse} contact={this.state.selectedWarehouse.contact} inventory={this.state.warehouseInventory} />
-      </div>
-    )
-  }
-
-}
-
-export default HomePage;
+  
+  export default HomePage;
