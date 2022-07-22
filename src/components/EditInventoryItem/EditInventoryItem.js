b9de146ed29a053a1372b7db137fd303ce98281d
Po Lin <po.ting.lin@gmail.com>
2022-07-21 22:30:11 -0400

    Add classes to elements on EditInventoryItem component

--- src/components/EditInventoryItem/EditInventoryItem.js
+++ src/components/EditInventoryItem/EditInventoryItem.js
@@ -18,20 +18,21 @@ function EditInventoryItem() {
 	
 	return (
 		<>
-			<header>
-				<h1>edit inventory item</h1>
+			<header className="edit-item__header">
+				<h1 className="edit-item__header-title">edit inventory item</h1>
 			</header>
-			<section>
-				<h2>item details</h2>
+			<section className="item-details__container">
 
-				<h3>item name</h3>
-				<input type="text" defaultValue={selectedItem.itemName}/>
+				<h2 className="item-details__title">item details</h2>
 
-				<h3>description</h3>
-				<textarea id="" name="" defaultValue={selectedItem.description}></textarea>
+				<h3 className="item-details__item-name-title">item name</h3>
+				<input className="item-details__item-name-input" type="text" id="name" name="name" defaultValue={selectedItem.itemName}/>
 
-				<h3>category</h3>
-				<select id="inventoryCategory" name="inventoryCategory">
+				<h3 className="item-details__description-title">description</h3>
+				<textarea className="item-details__description-input" id="description" name="description" defaultValue={selectedItem.description}></textarea>
+
+				<h3 className="item-details__category-title">category</h3>
+				<select className="item-details__category-select" id="inventoryCategory" name="inventoryCategory">
 					<option value={selectedItem.category}>{selectedItem.category}</option>				
 					{
 						uniqueCategories.map( category => {
@@ -41,29 +42,38 @@ function EditInventoryItem() {
 							return null;
 						})
 					}
-
 				</select>
-			</section>
-			<section>
-				<h2>item availability</h2>
-				<h3>status</h3>
-					<input 
-						type="radio" 
-						id="inStock" 
-						value="inStock" 
-						name="stock" 
-						defaultChecked={selectedInStock}
-					/>
-					<label htmlFor="inStock">In stock</label>
 
-					<input 
-						type="radio" 
-						id="outOfStock" 
-						value="outOfStock" 
-						name="stock" 
-						defaultChecked={!selectedInStock}
-					/>
-					<label htmlFor="outOfStock">Out of stock</label>
+			</section>
+			<section className="item-avail__container">
+
+				<h2 className="item-avail__title">item availability</h2>
+
+				<h3 className="item-avail__status-title">status</h3>
+					<label htmlFor="inStock">
+						<input 
+							className="item-avail__status-input"
+							type="radio" 
+							id="inStock" 
+							value="In Stock" 
+							name="inStock" 
+							defaultChecked={selectedInStock}
+						/>
+						In stock
+					</label>
+
+					<label htmlFor="outOfStock">
+						<input 
+							className="item-avail__status-input"
+							type="radio" 
+							id="outOfStock" 
+							value="Out of Stock" 
+							name="outOfStock" 
+							defaultChecked={!selectedInStock}
+						/>
+						Out of stock
+					</label>
+
 				<h3>warehouse</h3>
 					<select id="warehouseList" name="warehouseList">
 						<option value={selectedItem.warehouseName}>{selectedItem.warehouseName}</option>
@@ -78,9 +88,9 @@ function EditInventoryItem() {
 
 					</select>
 			</section>
-			<div>
-				<button name="cancel" type="reset">cancel</button>
-				<button name="save" type="submit">save</button>
+			<div className="edit-item__buttons-container">
+				<button className="edit-item__cancel-button" name="cancel" type="reset">cancel</button>
+				<button className="edit-item__submit-button" name="save" type="submit">save</button>
 			</div> 
 		</>
 	);
