84d7545422dd2d7058795c16ca483dfb067d7ac3
chelsia tong <chelsiatong@gmail.com>
2022-07-22 15:20:49 -0400

    completed posting

--- src/components/AddInventory/AddInventory.js
+++ src/components/AddInventory/AddInventory.js
@@ -5,15 +5,15 @@ import axios from 'axios';
 import { API_URL } from '../../App';
 import { Link } from 'react-router-dom';
 
+
 class AddInventory extends React.Component {
     state = {
         itemName: "",
         description: "",
-        // category: "",
-        status: "",
-        quantity: null,
+        status: "In stock",
+        quantity: "",
         warehouses: [],
-        // warehouseName: ""
+        invalidInput: false,
     }
 
     componentDidMount() {
@@ -34,17 +34,50 @@ class AddInventory extends React.Component {
         event.preventDefault();
         console.log(event);
 
+        if (event.target.categoryName.value === "") {
+            event.target.categoryName.classList.add("add-inventory__error");
+        } else {
+            event.target.categoryName.classList.remove("add-inventory__error");
+        }
+
+        if (event.target.warehouseName.value === "") {
+            event.target.warehouseName.classList.add("add-inventory__error");
+        } else {
+            event.target.warehouseName.classList.remove("add-inventory__error");
+        }
+
+        if (!this.state.itemName || !this.state.description || !this.state.status || (!this.state.quantity && this.state.status !== "Out of stock") || !event.target.warehouseName.value || !event.target.categoryName.value) {
+            this.setState({
+                invalidInput: true,
+            })
+            return;
+        }
+
+        if (Number(this.state.quantity) === NaN && this.state.quantity !== "") {
+            this.setState({
+                invalidInput: true,
+            })
+            return;
+        }
+        let newQuantity = this.state.quantity === "" ? 0 : (Number(this.state.quantity));
+
+        let newWarehouse = this.state.warehouses.find(warehouse => warehouse.id === event.target.warehouseID.value)
+
         axios
-            .post(`${API_URL}/inventory/add`, {
+            .post(`${API_URL}inventories`, {
+                warehouseID: event.target.warehouseID.value,
                 itemName: this.state.itemName,
                 description: this.state.description,
                 category: event.target.categoryName.value,
                 status: this.state.status,
-                quantity: this.state.quantity,
-                warehouseName: event.target.warehouseName.value,
+                quantity: newQuantity,
+                warehouseName: newWarehouse.name,
             })
             .then((response) => {
-                this.props.history.push('/');
+                this.props.history.push('/inventory');
+            })
+            .catch((error) => {
+                console.log('Error', error);
             })
     };
 
@@ -53,11 +86,6 @@ class AddInventory extends React.Component {
             [event.target.name]: event.target.value
         })
     }
-
-    //can i remove required?
-    //would it be this.state.warehouseID?? or the name...look at the inventories.json
-    //go to inventories.js and ask the question about the warehouseName: req.body.warehouseID
-
 
     render() {
         return (
@@ -71,41 +99,59 @@ class AddInventory extends React.Component {
                         <div className="add-inventory__inventory--detail" >
                             <h2 className="add-inventory__subtitle" >Item Details</h2>
                             <h3 className="add-inventory__labels" >Item Name</h3>
-                            <textarea type="text" placeholder="Item Name" value={this.state.itemName} onChange={this.handleChangeInventory} name="itemName" ></textarea>
+                            <textarea className={
+                                (this.state.itemName.length === 0 && this.state.invalidInput) ? "add-inventory__error" : ""} type="text" placeholder="Item Name" value={this.state.itemName} onChange={this.handleChangeInventory} name="itemName" ></textarea>
+                            <span className={
+                                (this.state.itemName.length === 0 && this.state.invalidInput) ? "add-inventory__warning" : "add-inventory__warning--hide"}>This field is required</span>
                             <h3 className="add-inventory__labels" >Description</h3>
-                            <textarea className="add-inventory__labels--description" type="text" placeholder="Please enter a brief item description..." value={this.state.description} onChange={this.handleChangeInventory} name="description" ></textarea>
+
+                            {/* className="add-inventory__labels--description" */}
+                            {/* rows="20" */}
+                            <textarea className={
+                                (this.state.description.length === 0 && this.state.invalidInput) ? "add-inventory__error add-inventory__labels--description " : "add-inventory__labels--description"} type="text" placeholder="Please enter a brief item description..." value={this.state.description} onChange={this.handleChangeInventory} name="description" ></textarea>
+                            <span className={
+                                (this.state.description.length === 0 && this.state.invalidInput) ? "add-inventory__warning" : "add-inventory__warning--hide"}>This field is required</span>
                             <h3 className="add-inventory__labels" >Category</h3>
                             <select name="categoryName" id="categoryName" className="add-inventory__dropdown">
-                                <option value="">Please Select</option>
+                                <option value="">Please select</option>
                                 <option value="Accessories" >Accessories</option>
                                 <option value="Apparel" >Apparel</option>
                                 <option value="Electronics" >Electronics</option>
                                 <option value="Health" >Health</option>
                                 <option value="Gear" >Gear</option>
                             </select>
+                            <span className={
+                                (this.state.invalidInput) ? "add-inventory__warning" : "add-inventory__warning--hide"}>This field is required</span>
                         </div>
                         <div className="add-inventory__availability--detail" >
-                            <h2 className="add-inventory__subtitle" >Contact Details</h2>
+                            <h2 className="add-inventory__subtitle" >Item Availability</h2>
                             <h3 className="add-inventory__labels" >Status</h3>
                             <div className="add-inventory__radio-buttons" >
                                 <div className="add-inventory__radio-buttons--in" >
-                                    <input type="radio" name="status" value={this.state.status} onChange={this.handleChangeWarehouse}></input>
+                                    <input type="radio" name="status" id="inStock" value="In stock" defaultChecked onChange={this.handleChangeInventory}></input>
                                     <label for="in">In stock</label>
                                 </div>
                                 <div>
-                                    <input type="radio" name="status" value={this.state.status} onChange={this.handleChangeWarehouse}></input>
+                                    <input type="radio" name="status" id="outOfStock" value="Out of stock" onChange={this.handleChangeInventory}></input>
                                     <label for="out">Out of stock</label>
                                 </div>
                             </div>
-                            <h3 className="add-inventory__labels" >Quantity</h3>
-                            <textarea type="text" name="quantity" placeholder="0" value={this.state.quantity} onChange={this.handleChangeWarehouse}  ></textarea>
+                            <div className={(this.state.status === "Out of stock") ? "add-inventory__quantity--hide" : ""} >
+                                <h3 className="add-inventory__labels" >Quantity</h3>
+                                <textarea className={
+                                    (!this.state.quantity && this.state.invalidInput) ? "add-inventory__error" : ""} type="number" name="quantity" placeholder="0" value={this.state.quantity} onChange={this.handleChangeInventory}  ></textarea>
+                                <span className={
+                                    (this.state.quantity.length === 0 && this.state.invalidInput) ? "add-inventory__warning" : "add-inventory__warning--hide"}>This field is required</span>
+                            </div>
                             <h3 className="add-inventory__labels" >Warehouse</h3>
-                            <select name="warehouseName" id="warehouseName" className="add-inventory__dropdown">
-                                <option value="">Please Select</option>
+                            <select name="warehouseID" id="warehouseName" className="add-inventory__dropdown">
+                                <option value="">Please select</option>
                                 {this.state.warehouses.map((eachWarehouse) => (
-                                    <option key={eachWarehouse.Id} value={eachWarehouse.name}>{eachWarehouse.name}</option>
+                                    <option key={eachWarehouse.Id} value={eachWarehouse.id}>{eachWarehouse.name}</option>
                                 ))}
                             </select>
+                            <span className={
+                                (this.state.invalidInput) ? "add-inventory__warning" : "add-inventory__warning--hide"}>This field is required</span>
                         </div>
                     </div>
                     <div className="add-inventory__submit" >
