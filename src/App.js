158ced7c33940e334820c085cb069a66a6a36223
stephonduong <stephonduong@gmail.com>
2022-07-23 14:09:14 -0400

    fixed api url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import InventoryPage from './pages/InventoryPage/InventoryPage';
 
-export const API_URL = 'http://localhost:8080/';
+export const API_URL = 'http://localhost:8080';
 
 function App() {
     return (
