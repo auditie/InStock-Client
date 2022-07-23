98345766422fe1579fbe460ab763fe14080b22d4
Po Lin <po.ting.lin@gmail.com>
2022-07-23 16:48:53 -0400

    Style delete modal thru tablet sizes

--- src/components/DeleteWarehouse/DeleteWarehouse.js
+++ src/components/DeleteWarehouse/DeleteWarehouse.js
@@ -1,15 +1,16 @@
 import './DeleteWarehouse.scss';
+import closeButton from '../../assets/icons/close-24px.svg';
 
 const DeleteWarehouse = ({ warehouseName, hideDeleteWarehouse, handleDeleteWarehouse }) => {
 	return(
 		<div className="delete-warehouse">
 			<div className="delete-warehouse__card">
-				<button onClick={hideDeleteWarehouse} className="delete-warehouse__x-button"></button>
-				<h1>{`Delete ${warehouseName} warehouse`}</h1>
-				<p>{`Please confirm that you'd like to delete the ${warehouseName} from the list of warehouses. You won't be able to undo this action.`}</p>
-				<div className="delete-warehouse__buttons-containter">
-					<button onClick={hideDeleteWarehouse}>cancel</button>
-					<button onClick={handleDeleteWarehouse}>delete</button>
+				<img src={closeButton} onClick={hideDeleteWarehouse} className="delete-warehouse__x-button"></img>
+				<h1 className="delete-warehouse__title">{`Delete ${warehouseName} warehouse?`}</h1>
+				<p className="delete-warehouse__text">{`Please confirm that you'd like to delete the ${warehouseName} from the list of warehouses. You won't be able to undo this action.`}</p>
+				<div className="delete-warehouse__buttons-container">
+					<button className="delete-warehouse__button-cancel" onClick={hideDeleteWarehouse}>cancel</button>
+					<button className="delete-warehouse__button-delete" onClick={handleDeleteWarehouse}>delete</button>
 				</div>
 			</div>
 		</div>
