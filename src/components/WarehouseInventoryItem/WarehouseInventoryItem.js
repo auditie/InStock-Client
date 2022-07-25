608ab3ae0de4d61b83e43597201ca959e4a0d056
Po Lin <po.ting.lin@gmail.com>
2022-07-24 20:28:15 -0400

    Add hover styling for delete icon in WarehouseInventoryItem

--- src/components/WarehouseInventoryItem/WarehouseInventoryItem.js
+++ src/components/WarehouseInventoryItem/WarehouseInventoryItem.js
@@ -38,7 +38,7 @@ const WarehouseInventoryItem = (props) => {
       </div>
       </article>
       <div className='warehouse-inventory__actions'>
-        <img onClick={(e) => {showDeleteInventory(props.id);}} src={trash} alt='delete item'/>
+        <img className='warehouse-inventory__delete' onClick={(e) => {showDeleteInventory(props.id);}} src={trash} alt='delete item'/>
         <Link className="warehouse-inventory__link" to={`/inventory/${props.id}/edit`}></Link>
       </div>
     </section>
